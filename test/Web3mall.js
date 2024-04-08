a4bce4c26865a506b47d5513ed969a4deadf72d7
vishal-k25 <vishalkhekade25@gmail.com>
2024-04-09 00:43:51 +0530

    updated tests

--- test/Web3mall.js
+++ test/Web3mall.js
@@ -1,6 +1,7 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require("chai"); // Importing Chai assertion library for testing
+const { ethers } = require("hardhat"); // Importing ethers for Ethereum interactions
 
+// Helper function to convert ether to token units
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
@@ -36,95 +37,100 @@ describe("Web3mall", () => {
   });
 
   describe("Listing", () => {
-    let transaction
+    let transaction;
 
     beforeEach(async () => {
       // List a item
-      transaction = await web3mall.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
-      await transaction.wait()
-    })
+      transaction = await web3mall
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+    });
 
     it("Returns item attributes", async () => {
-      const item = await web3mall.items(ID)
+      const item = await web3mall.items(ID);
 
-      expect(item.id).to.equal(ID)
-      expect(item.name).to.equal(NAME)
-      expect(item.category).to.equal(CATEGORY)
-      expect(item.image).to.equal(IMAGE)
-      expect(item.cost).to.equal(COST)
-      expect(item.rating).to.equal(RATING)
-      expect(item.stock).to.equal(STOCK)
-    })
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+    });
 
     it("Emits List event", () => {
-      expect(transaction).to.emit(web3mall, "List")
-    })
-  })
+      expect(transaction).to.emit(web3mall, "List");
+    });
+  });
 
   describe("Buying", () => {
-    let transaction
+    let transaction;
 
     beforeEach(async () => {
       // List a item
-      transaction = await web3mall.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
-      await transaction.wait()
+      transaction = await web3mall
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
 
       // Buy a item
-      transaction = await web3mall.connect(buyer).buy(ID, { value: COST })
-      await transaction.wait()
-    })
-
+      transaction = await web3mall.connect(buyer).buy(ID, { value: COST });
+      await transaction.wait();
+    });
 
     it("Updates buyer's order count", async () => {
-      const result = await web3mall.orderCount(buyer.address)
-      expect(result).to.equal(1)
-    })
+      const result = await web3mall.orderCount(buyer.address);
+      expect(result).to.equal(1);
+    });
 
     it("Adds the order", async () => {
-      const order = await web3mall.orders(buyer.address, 1)
+      const order = await web3mall.orders(buyer.address, 1);
 
-      expect(order.time).to.be.greaterThan(0)
-      expect(order.item.name).to.equal(NAME)
-    })
+      expect(order.time).to.be.greaterThan(0);
+      expect(order.item.name).to.equal(NAME);
+    });
 
     it("Updates the contract balance", async () => {
-      const result = await ethers.provider.getBalance(web3mall.address)
-      expect(result).to.equal(COST)
-    })
+      const result = await ethers.provider.getBalance(web3mall.address);
+      expect(result).to.equal(COST);
+    });
 
     it("Emits Buy event", () => {
-      expect(transaction).to.emit(web3mall, "Buy")
-    })
-  })
+      expect(transaction).to.emit(web3mall, "Buy");
+    });
+  });
 
   describe("Withdrawing", () => {
-    let balanceBefore
+    let balanceBefore;
 
     beforeEach(async () => {
       // List a item
-      let transaction = await web3mall.connect(deployer).list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
-      await transaction.wait()
+      let transaction = await web3mall
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
 
       // Buy a item
-      transaction = await web3mall.connect(buyer).buy(ID, { value: COST })
-      await transaction.wait()
+      transaction = await web3mall.connect(buyer).buy(ID, { value: COST });
+      await transaction.wait();
 
       // Get Deployer balance before
-      balanceBefore = await ethers.provider.getBalance(deployer.address)
+      balanceBefore = await ethers.provider.getBalance(deployer.address);
 
       // Withdraw
-      transaction = await web3mall.connect(deployer).withdraw()
-      await transaction.wait()
-    })
+      transaction = await web3mall.connect(deployer).withdraw();
+      await transaction.wait();
+    });
 
-    it('Updates the owner balance', async () => {
-      const balanceAfter = await ethers.provider.getBalance(deployer.address)
-      expect(balanceAfter).to.be.greaterThan(balanceBefore)
-    })
+    it("Updates the owner balance", async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address);
+      expect(balanceAfter).to.be.greaterThan(balanceBefore);
+    });
 
-    it('Updates the contract balance', async () => {
-      const result = await ethers.provider.getBalance(web3mall.address)
-      expect(result).to.equal(0)
-    })
-  })
-})
+    it("Updates the contract balance", async () => {
+      const result = await ethers.provider.getBalance(web3mall.address);
+      expect(result).to.equal(0);
+    });
+  });
+});
