063ec996a516cf7d2717ff8fdaece7b85641b860
vishal-k25 <vishalkhekade25@gmail.com>
2024-04-09 00:08:48 +0530

    Implemented Ethereum blockchain integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation'
-import Section from './components/Section'
-import Product from './components/Product'
+import Navigation from "./components/Navigation";
+import Section from "./components/Section";
+import Product from "./components/Product";
 
 // ABIs
-import Dappazon from './abis/Dappazon.json'
+import Web3mall from "./abis/web3mall.json";
 
 // Config
-import config from './config.json'
+import config from "./config.json";
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [web3mall, setWeb3mall] = useState(null);
+
+  const [account, setAccount] = useState(null);
+
+  const [electronics, setElectronics] = useState(null);
+  const [clothing, setClothing] = useState(null);
+  const [toys, setToys] = useState(null);
+
+  const [item, setItem] = useState({});
+  const [toggle, setToggle] = useState(false);
+
+  const togglePop = (item) => {
+    setItem(item);
+    toggle ? setToggle(false) : setToggle(true);
+  };
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+
+    const web3mall = new ethers.Contract(
+      config[network.chainId].web3mall.address,
+      Web3mall,
+      provider
+    );
+    setWeb3mall(web3mall);
+
+    const items = [];
+
+    for (var i = 0; i < 9; i++) {
+      const item = await web3mall.items(i + 1);
+      items.push(item);
+    }
+
+    const electronics = items.filter((item) => item.category === "electronics");
+    const clothing = items.filter((item) => item.category === "clothing");
+    const toys = items.filter((item) => item.category === "toys");
+
+    setElectronics(electronics);
+    setClothing(clothing);
+    setToys(toys);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
+      
+      <h2>Web3mall Best Sellers</h2>
 
-      <h2>Welcome to Dappazon</h2>
+      {electronics && clothing && toys && (
+        <>
+          <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop}/>
+          <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
 
+      {toggle && (
+        <Product
+          item={item}
+          provider={provider}
+          account={account}
+          web3mall={web3mall}
+          togglePop={togglePop}
+        />
+      )}
     </div>
   );
 }
